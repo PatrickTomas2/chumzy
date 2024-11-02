d33baa9fc92fc44f33d147a4c7543e8941ae2051
Patrick-028 <patrickjohntomas0228@gmail.com>
2024-11-02 14:35:25 +0800

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:chumzy/app.dart';
+import 'package:chumzy/core/firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(ChumzyApp());
 }
